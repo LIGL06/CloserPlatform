cc8aae536ce5ec68669a8980076f96940b43f17f
Luis Iván García Luna <Luis Ivn Garca Luna>
2016-01-06 00:53:44 -0600

    Final

--- test.js
+++ test.js
@@ -85,4 +85,79 @@ describe('User Journaul', function(){
         })
       })
     })
+    it('can save users journal',function(done){
+      const url = URL_ROOT + '/me/journal'
+      superagent.put(utl).send({
+          data: {
+            cart:[{challenge_ID, quantity: 1}]
+          }
+        }).
+        end(function(error,res){
+          assert.ifError(error)
+          assert.equal(res.status, status.OK)
+          User.findOne({}, function(error,user){
+            assert.ifError(error)
+            assert.equal(user.data.journal.length,1)
+            assert.equal(user.data.cart[0].challenge, challenge_ID)
+            assert.equal(user.data.cart[0].quantity, 1)
+            done()
+          })
+        })
+    })
+    it('can load users journal',function(done){
+      const url = URL_ROOT + '/me'
+
+      User.findOne({},function(error,user){
+        assert.ifError(error)
+        user.data.journal = [{ challenge: challenge_ID, quantity: 1}]
+        user.save(function(error){
+          assert.ifError(error)
+          superagent.get(url,function(error,res){
+            assert.ifError(error)
+            assert.equal(res.status, 200)
+            const result
+            assert.doesNotThrow(function(){
+              result = JSON.parse(res.text).user
+            })
+            assert.equal(result.data.journal.length, 1)
+            assert.equal(result.data.journal[0].challenge.name, '30 minutes Challenge')
+            assert.equal(result.data.journal[0].quantity, 1)
+            done()
+          })
+        })
+      })
+    })
+    it('can check out',function(done){
+      const url = URL_ROOT + '/checkout'
+
+      User.findOne({},function(error,user){
+        asert.ifError(error)
+        user.data.journal = [{ product: challenge_ID, quantify: 1 }]
+        user.save(function(error){
+          assert.ifError(error)
+          superagent.post(url).send({
+            stripeToken: {
+              number: '4242424242424242',
+              cvc: '123',
+              exp_month: '12',
+              exp_yar: '2016'
+            }
+          }).end(function(error,res){
+            assert.ifError(error)
+            assert.equal(res.status, 200)
+            const result
+            assert.doesNotThrow(function(){
+              result = JSON.parse(res.text)
+            })
+            assert.ok(result.id)
+            Stripe.charges.retrieve(result.id,function(error,charge){
+              assert.ifError(error)
+              assert.ok(charge)
+              assert.equal(charge.amount, 100)
+              done()
+            })
+          })
+        })
+      })
+    })
 })
